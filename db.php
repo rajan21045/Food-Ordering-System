de1d7f9a35af140c7f926dc58e9dd0815377c7a8
Rajan Poudel <raajan.works@gmail.com>
2026-04-28 22:08:47 +0545

    Update db.php

--- db.php
+++ db.php
@@ -5,7 +5,7 @@ $password = "*rajan12345#";
 $dbname = "foodOrderingSystem";
 
 $conn = mysqli_connect($severname, $username, $password, $dbname);
-
+ 
 if($conn->connect_error){
     die("Connection failed: " . $conn->connect_error);
 }
